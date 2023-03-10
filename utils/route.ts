55fad1a696111a13b6203d6422d5445e63709615
LetTTGACO <1225751694@qq.com>
2023-03-10 22:39:09 +0800

    feat(elog): 升级elog

--- utils/route.ts
+++ utils/route.ts
@@ -1,4 +1,38 @@
-export const genSideBar = () => {
-  return require('../router.json')
+function genRoute(arr, pathname) {
+  function loop(parId) {
+    return arr.reduce((acc, cur) => {
+      if (cur.parent_uuid === parId) {
+        const parent = arr.find(item => item.uuid === parId)
+        cur.path = (parent?.path || '') + '/' + cur.title
+        cur.items = loop(cur.uuid)
+        let route
+        if (cur.items.length) {
+          route = {
+            text: cur.title,
+            collapsible: true,
+            items: cur.items
+          }
+          acc.push(route)
+        } else {
+          if (cur.type === 'DOC') {
+            route = {
+              text: cur.title,
+              link: `${pathname}${cur.path}`,
+            }
+            acc.push(route)
+          }
+        }
+      }
+      return acc
+    }, [])
+  }
+  return loop('')
+}
+
+
+export const genSideBar = (pathname: string) => {
+  const cache = require('../elog-cache.json')
+  const { catalog } = cache
+  return genRoute(catalog, pathname)
 }
 
