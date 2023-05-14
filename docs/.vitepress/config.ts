59dc93131fd6e068d333d36aeb90b7ca481bd1b7
LetTTGACO <1225751694@qq.com>
2023-05-14 15:01:04 +0800

    feat: 升级elog

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -59,7 +59,7 @@ export default defineConfig({
       { text: 'Notion版', link: '/notion/start', activeMatch: '/notion/' },
       { text: 'Elog 开发计划', link: 'https://1874.notion.site/Elog-91dd2037c9c847e6bc90b712b124189c' },
       {
-        text: 'V0.4.2',
+        text: 'V0.4.3',
         items: [
           {
             text: 'Changelog',
