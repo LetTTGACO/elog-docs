dd86cd865d02f5eff099079e4246abb0b811644c
LetTTGACO <1225751694@qq.com>
2023-10-09 17:04:04 +0800

    feat: 升级elog
    1.支持飞书云文档

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -68,7 +68,7 @@ export default defineConfig({
       },
       { text: 'Elog 开发计划', link: 'https://1874.notion.site/Elog-91dd2037c9c847e6bc90b712b124189c' },
       {
-        text: 'V0.8.0-beta.0',
+        text: 'V0.8.0-beta.1',
         items: [
           {
             text: 'Changelog',
