60e1d19f67ec2c40feb67683d2edaf81631d3e1c
LetTTGACO <1225751694@qq.com>
2024-01-26 20:51:11 +0800

    feat: elog-docs增加评论

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,38 @@
+// .vitepress/theme/index.js
+import DefaultTheme from 'vitepress/theme';
+import giscusTalk from 'vitepress-plugin-comment-with-giscus';
+import { useData, useRoute } from 'vitepress';
+export default {
+  ...DefaultTheme,
+  enhanceApp(ctx) {
+    DefaultTheme.enhanceApp(ctx);
+    // ...
+  },
+  setup() {
+    // 获取前言和路由
+    const { frontmatter } = useData();
+    const route = useRoute();
+
+    // 评论组件 - https://giscus.app/
+    giscusTalk({
+        repo: 'LetTTGACO/elog-docs',
+        repoId: 'R_kgDOIh13_Q',
+        category: 'General', // 默认: `General`
+        categoryId: 'DIC_kwDOIh13_c4CbnHJ',
+        mapping: 'pathname', // 默认: `pathname`
+        inputPosition: 'top', // 默认: `top`
+        lang: 'zh-CN', // 默认: `zh-CN`
+        lightTheme: 'light', // 默认: `light`
+        darkTheme: 'transparent_dark', // 默认: `transparent_dark`
+        // ...
+      }, {
+        frontmatter, route
+      },
+      // 是否全部页面启动评论区。
+      // 默认为 true，表示启用，此参数可忽略；
+      // 如果为 false，表示不启用。
+      // 可以在页面使用 `comment: true` 前言单独启用
+      true
+    );
+  }
+};
